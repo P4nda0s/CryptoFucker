ddf6d8b69c0441e127fc59827b20ecbc05d1a39e
xiaoq <xiaoq.py@gmail.com>
2021-04-22 17:12:25 +0800

    add rsa;fix MessageDigest.digest

--- app/src/main/java/com/example/a14473/xp/HexDumper.java
+++ app/src/main/java/com/example/a14473/xp/HexDumper.java
@@ -1,24 +1,20 @@
 package com.example.a14473.xp;
 
-public class HexDumper
-{
+public class HexDumper {
 
-    private final static char[] HEX_DIGITS = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+    private final static char[] HEX_DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-    public static String dumpHexString(byte[] array)
-    {
+    public static String dumpHexString(byte[] array) {
         if (array.equals(null))
             return "null";
 
         byte[] byte2 = new byte[array.length + 0x10];
-        for(int i = 0;i < byte2.length ;i++)
-        {
+        for (int i = 0; i < byte2.length; i++) {
             byte2[i] = 0;
         }
 
         // --* . *--
-        for(int i=0;i<array.length;i++)
-        {
+        for (int i = 0; i < array.length; i++) {
             byte2[i] = array[i];
         }
 
@@ -26,8 +22,7 @@ public class HexDumper
         return dumpHexString(byte2, 0, byte2.length);
     }
 
-    public static String dumpHexString(byte[] array, int offset, int length)
-    {
+    public static String dumpHexString(byte[] array, int offset, int length) {
         StringBuilder result = new StringBuilder();
 
         byte[] line = new byte[16];
@@ -36,20 +31,14 @@ public class HexDumper
         result.append("\n0x");
         result.append(toHexString(offset));
 
-        for (int i = offset ; i < offset + length ; i++)
-        {
-            if (lineIndex == 16)
-            {
+        for (int i = offset; i < offset + length; i++) {
+            if (lineIndex == 16) {
                 result.append(" ");
 
-                for (int j = 0 ; j < 16 ; j++)
-                {
-                    if (line[j] > ' ' && line[j] < '~')
-                    {
+                for (int j = 0; j < 16; j++) {
+                    if (line[j] > ' ' && line[j] < '~') {
                         result.append(new String(line, j, 1));
-                    }
-                    else
-                    {
+                    } else {
                         result.append(".");
                     }
                 }
@@ -67,23 +56,17 @@ public class HexDumper
             line[lineIndex++] = b;
         }
 
-        if (lineIndex != 16)
-        {
+        if (lineIndex != 16) {
             int count = (16 - lineIndex) * 3;
             count++;
-            for (int i = 0 ; i < count ; i++)
-            {
+            for (int i = 0; i < count; i++) {
                 result.append(" ");
             }
 
-            for (int i = 0 ; i < lineIndex ; i++)
-            {
-                if (line[i] > ' ' && line[i] < '~')
-                {
+            for (int i = 0; i < lineIndex; i++) {
+                if (line[i] > ' ' && line[i] < '~') {
                     result.append(new String(line, i, 1));
-                }
-                else
-                {
+                } else {
                     result.append(".");
                 }
             }
@@ -92,23 +75,19 @@ public class HexDumper
         return result.toString();
     }
 
-    public static String toHexString(byte b)
-    {
+    public static String toHexString(byte b) {
         return toHexString(toByteArray(b));
     }
 
-    public static String toHexString(byte[] array)
-    {
+    public static String toHexString(byte[] array) {
         return toHexString(array, 0, array.length);
     }
 
-    public static String toHexString(byte[] array, int offset, int length)
-    {
+    public static String toHexString(byte[] array, int offset, int length) {
         char[] buf = new char[length * 2];
 
         int bufIndex = 0;
-        for (int i = offset ; i < offset + length; i++)
-        {
+        for (int i = offset; i < offset + length; i++) {
             byte b = array[i];
             buf[bufIndex++] = HEX_DIGITS[(b >>> 4) & 0x0F];
             buf[bufIndex++] = HEX_DIGITS[b & 0x0F];
@@ -117,47 +96,41 @@ public class HexDumper
         return new String(buf);
     }
 
-    public static String toHexString(int i)
-    {
+    public static String toHexString(int i) {
         return toHexString(toByteArray(i));
     }
 
-    public static byte[] toByteArray(byte b)
-    {
+    public static byte[] toByteArray(byte b) {
         byte[] array = new byte[1];
         array[0] = b;
         return array;
     }
 
-    public static byte[] toByteArray(int i)
-    {
+    public static byte[] toByteArray(int i) {
         byte[] array = new byte[4];
 
-        array[3] = (byte)(i & 0xFF);
-        array[2] = (byte)((i >> 8) & 0xFF);
-        array[1] = (byte)((i >> 16) & 0xFF);
-        array[0] = (byte)((i >> 24) & 0xFF);
+        array[3] = (byte) (i & 0xFF);
+        array[2] = (byte) ((i >> 8) & 0xFF);
+        array[1] = (byte) ((i >> 16) & 0xFF);
+        array[0] = (byte) ((i >> 24) & 0xFF);
 
         return array;
     }
 
-    private static int toByte(char c)
-    {
+    private static int toByte(char c) {
         if (c >= '0' && c <= '9') return (c - '0');
         if (c >= 'A' && c <= 'F') return (c - 'A' + 10);
         if (c >= 'a' && c <= 'f') return (c - 'a' + 10);
 
-        throw new RuntimeException ("Invalid hex char '" + c + "'");
+        throw new RuntimeException("Invalid hex char '" + c + "'");
     }
 
-    public static byte[] hexStringToByteArray(String hexString)
-    {
+    public static byte[] hexStringToByteArray(String hexString) {
         int length = hexString.length();
         byte[] buffer = new byte[length / 2];
 
-        for (int i = 0 ; i < length ; i += 2)
-        {
-            buffer[i / 2] = (byte)((toByte(hexString.charAt(i)) << 4) | toByte(hexString.charAt(i+1)));
+        for (int i = 0; i < length; i += 2) {
+            buffer[i / 2] = (byte) ((toByte(hexString.charAt(i)) << 4) | toByte(hexString.charAt(i + 1)));
         }
 
         return buffer;
